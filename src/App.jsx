401313781cac21c82e27549d358f3f17d97c834c
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-12-12 12:46:07 -0800

    started using switch on the view

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ import MovieList from "./components/MovieList";
 import GameList from "./components/GameList";
 import ShowList from "./components/ShowList";
 
+const pages = [
+  'Home Page',
+  'Create a New Media',
+  'Index Page', //will contain the delete functionality and update functionality
+  'Abous Us'
+]
+
 const App = () => {
   const [mediaItems, setMediaItems] = useState([]);
   const [search, setSearch] = useState("");
+  const [content, setContent] = useState(pages[0])
 
   // here we useEffect to have things load on opening the page
   useEffect(() => {
@@ -23,22 +31,33 @@ const App = () => {
     fetchMediaItems();
   }, []);
 
-  return (
+  switch (content) {
+    case 'Create a New Media':
+      return <MediaForm />
+    default:
+      return (
 
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      {/* search box component go here */}
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {/* search box component go here */}
+    
+          {/* logo goes here */}
+          <img style={{ width: '10rem' }}
+           src="/defaults/entertainment_hub.png"
+           alt="/defaults/entertainment_hub.png"
+           />
+          <button onClick={() => setContent('Create a New Media')}>Add new media</button>
+    
+          <BookList items={mediaItems.filter((item) => item.type === 'book')} />
+          <MovieList items={mediaItems.filter((item) => item.type ==='movie')} />
+          <GameList items={mediaItems.filter((item) => item.type === 'game')} />
+          <ShowList items={mediaItems.filter((item) => item.type === 'show')} />
+    
+        </div>
+      );
+      break;
+  }
 
-      {/* logo goes here */}
-      <img style={{ width: '10rem' }} src="/defaults/entertainment_hub.png" alt="/defaults/entertainment_hub.png" />
-      <button>Add new media</button>
-
-      <BookList items={mediaItems.filter((item) => item.type === 'book')} />
-      <MovieList items={mediaItems.filter((item) => item.type ==='movie')} />
-      <GameList items={mediaItems.filter((item) => item.type === 'game')} />
-      <ShowList items={mediaItems.filter((item) => item.type === 'show')} />
-
-    </div>
-  );
+  
 };
 
 export default App;
