a1ea35d6108bed4ef713eb5b0136d728437b6f3c
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-12-13 13:59:25 -0800

    minor changes on form exit button where i change the opacity made Card bigger

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -19,12 +19,12 @@ const Card = ({ item }) => {
       <div
         className="ui card"
         style={{
-          width: "150px",
+          width: "160px",
           backgroundColor: "var(--ui-background-color)",
           cursor: 'pointer'
         }}
       >
-        <div className="image" style={{ height: "150px" }}>
+        <div className="image" style={{ height: "200px" }}>
           {item.img_url ? (
             // design how it looks like later
             <img
