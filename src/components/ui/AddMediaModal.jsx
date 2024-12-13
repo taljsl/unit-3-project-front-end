555eea0fd8e3e4ad006c340feca8af2a94e8ba6b
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-12-13 00:27:52 -0800

    added support for light theme and dark theme depending on users device setting added AddMediaModal

---
+++ src/components/ui/AddMediaModal.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+
+import './AddMediaModal.css'
+
+const AddMediaModal = ({ isOpen=false, onClose, children }) => {
+  
+  return (
+    <div
+     className='modal'
+     style={{ display: isOpen ? 'block' : 'none' }}
+    >
+        <div className='modal_content'>
+            <i className="close icon" onClick={() => onClose() }></i>
+
+            <h3>Add new Media</h3>
+            <div>
+                {children}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddMediaModal
